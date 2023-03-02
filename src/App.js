98c8ee1ab102c250bb91813fd319c428da6f36aa
Gordon Jooste <jooste44@gmail.com>
2023-03-02 18:31:49 +0700

    got the clicks working with errors. made the different columns into lists with keys.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
+
+  const projects = [ {title: 'Project 1', tasks: {tasks: [ {title: 'task1', description: 'task 1 description is nice and long', complete: true, date: new Date(2023,3,2) }, {title: 'task2', description: 'task 2 description is nice and long', complete: false, date: new Date(2023,3,2) }], total: 2, completed: 1 } }, {title: 'Project 2', tasks: {tasks: [ {title: 'p2task1', description: 'Project2 task 1 description is nice and long', complete: true, date: new Date(2023,3,2) }, {title: 'P2task2', description: 'Project 2 task 2 description is nice and long', complete: false, date: new Date(2023,3,2) }], total: 2, completed: 1 } }];
+  const selectedProject = projects[0]
+  const tasks = selectedProject.tasks.tasks;
+  const selectedDate = new Date(Date.now());
+  const {day1,day2,day3,day4,day5} = { day1: new Date(selectedDate.getTime()+100000000),day2:new Date(selectedDate.getTime()+200000000),day3:new Date(selectedDate.getTime()+300000000),day4:new Date(selectedDate.getTime()+400000000),day5: new Date(selectedDate.getTime()+500000000)};
+  const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  let checked = false;
+
+  const handleClick = (e)=>{
+    console.log('you clicked the box!');
+    return !checked;
+  }
+
+  //make the checkboxes work
+  // set up the state and redux store
+  //connect redux store and state handle changes to make the site dynamic. 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+
+      <div className='Projects' >
+      <h3> Projects </h3>
+        <div className = 'column'>  
+          <ul>
+           {projects.map( (project) => { 
+          return (
+            <li key={project.title}> 
+              <h4> {project.title}</h4>
+              <article> There are {project.tasks.total} tasks</article>
+            </li>
+                )
+              }) 
+            }
+          </ul>
+        </div>
+      </div> 
+
+      <div className='Todo'>
+          <h3>All Tasks for today</h3>
+          <div className='column'>
+          <div>
+              TODO Add Task
+          </div>
+            <ul>
+            {tasks.map(task =>{
+              return (
+                <li key={task.title}>
+                  <input type="checkbox" checked={checked} onClick={handleClick()}/>
+                  <h5>{task.title}</h5>
+                  <article>{task.description}</article>
+                  <article>TODO: Time of task</article>
+                </li>
+                )
+            })}
+            </ul>
+            <div className='statistics'>
+              <h4>{(selectedProject.tasks.completed/selectedProject.tasks.total)*100} % Completed</h4>
+            </div>
+          </div>
+      </div>
+
+      <div className='Calendar'>
+        <h3> Calendar</h3>
+        <h4> {selectedDate.toLocaleString('default', { month: 'long' }) } </h4>
+          <div className='column'>
+            <ul>
+              <li key={selectedDate.getDate()}>
+                <h4>{ selectedDate.getDate() }</h4>
+                <article> {weekday[selectedDate.getDay()]} </article>
+              </li>
+              <li key={day1.getDate()}>
+                <article> { day1.getDate() }  { weekday[day1.getDay()]}</article>
+              </li>
+              <li key={day2.getDate()}>
+                <article> { day2.getDate() }  { weekday[day2.getDay()]}</article>
+              </li>
+              <li key={day3.getDate()}>
+                <article> { day3.getDate() }  { weekday[day3.getDay()]}</article>  
+              </li>
+              <li key={day4.getDate()}>
+                <article> { day4.getDate() }  { weekday[day4.getDay()]}</article>  
+              </li>
+              <li key={day5.getDate()}>
+                <article> { day5.getDate() }  { weekday[day5.getDay()]}</article>  
+              </li>
+            </ul>
+              
+          </div>
+      </div>
+
     </div>
   );
 }
